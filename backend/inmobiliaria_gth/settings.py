193e247e39704f25d05edc003d770135079b2759
Leandro2407 <arandakenap24@gmail.com>
2026-04-20 21:39:27 -0300

    cambio BD a MySQL

--- backend/inmobiliaria_gth/settings.py
+++ backend/inmobiliaria_gth/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'inmobiliaria_gth.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inmobiliaria_gth',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
