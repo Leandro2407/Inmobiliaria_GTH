d1fbe2b1d76fcae506853a16b01e840664dae90b
Leandro2407 <arandakenap24@gmail.com>
2026-04-23 12:19:18 -0300

    backend

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Leandro\Desktop\Inmobiliaria_GTH\venv"
+set "VIRTUAL_ENV=C:\Users\K3nUp\Desktop\Inmobiliaria_GTH\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
